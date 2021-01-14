7c1220ca755dbaa33cf2eb97a26b1687ff73eda3
Dawson Jackson <dawsonjackson@Dawsons-MacBook-Pro.local>
2021-01-14 10:26:23 -0800

    signup prototype

--- main_app/views.py
+++ main_app/views.py
@@ -4,10 +4,30 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
+
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
+def about(request):
+    return render(request, 'about.html')
 
 
+# =============== Signup Route ================
 
+def signup(request):
+    error_message = ''
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid Username or Password'
+
+
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'trips/signup.html', context)
