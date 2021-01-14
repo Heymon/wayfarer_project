f8d5401909d467c628dca51361ffe5db3423fd34
Dawson Jackson <dawsonjackson@Dawsons-MacBook-Pro.local>
2021-01-14 14:43:45 -0800

    titles about.html and resolves conflicts

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,17 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     #home route
     path('', views.home, name='home'),
+
 
     path('about/', views.about),
 
     path('accounts/signup', views.signup, name='signup'),
-]
-=======
+
     path('', views.home, name='home'),
+
     path('', views.profile, name='profile')
 ]
 
->>>>>>> master
