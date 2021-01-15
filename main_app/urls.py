4d5aa042c075d742195783581803b76cdd1a11b2
Dawson Jackson <dawsonjackson@Dawsons-MacBook-Pro.local>
2021-01-14 16:03:54 -0800

    resolves coflicts

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,14 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     #home route
     path('', views.home, name='home'),
+
     path('accounts/signup', views.signup, name='signup'),
-]
-=======
-    path('', views.home, name='home'),
+    
     path('', views.profile, name='profile')
 ]
 
->>>>>>> master
