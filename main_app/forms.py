ea4a55d0f1916a2c3c9c96ce847670cadadde8f4
Heymon <rcavalleiro@gmail.com>
2021-01-15 10:58:03 -0800

    creates Profile form and adjusts user form

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,15 +1,18 @@
+from main_app.models import Profile
 from django.forms import ModelForm, fields
 
 
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 from django import forms
 
 class UserProfileForm(UserCreationForm):
     email = forms.EmailField(label= "Email")
     first_name = forms.CharField(label= "First Name")
     last_name = forms.CharField(label= "Last Name")
+    # cur_country = forms.CharField(label= "Current Country")
 
     class Meta:
         model = User
@@ -17,10 +20,16 @@ class UserProfileForm(UserCreationForm):
 
     def save(self, commit=True):
         user = super(UserProfileForm, self).save(commit=False)
-        user.first_name =self.cleaned_data["first_name"]
-        user.last_name =self.cleaned_data["last_name"]
-        user.email =self.cleaned_data["email"]
-        # user.set_password(self.cleaned_data["password1"])
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.email = self.cleaned_data["email"]
+        # user.objects.profile_set.all()[0].cur_country = self.cleaned_data["cur_country"]
         if commit:
             user.save()
         return user
+
+class Profile_Form(ModelForm):
+    class Meta:
+        model = Profile
+        labels = {'cur_country': 'Current Country'}
+        fields = ['cur_country',]
