433bf144f29455330d6dda903fe96c0265a005a0
jonathanespana <tempe.espana@gmail.com>
2021-01-15 15:17:36 -0700

    added signup modal to homepage

--- main_app/views.py
+++ main_app/views.py
@@ -9,7 +9,30 @@ from django.contrib.auth import login
 # Create your views here.
 
 def home (request):
-    return render(request, 'home.html')
+    error = ''
+    if request.method == 'POST':
+        mutable_POST = request.POST.copy()
+        user_country = QueryDict(f'cur_country={mutable_POST.pop("cur_country")[0]}')
+
+        print(request.POST["username"])
+        user_form = UserProfileForm(mutable_POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            #creates profile/connects to user
+            profile = Profile_Form(user_country)
+            user_profile = profile.save(commit=False)
+            user_profile.user = user
+            user_profile.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error = user_form.errors
+            print(error)
+    
+    user_form = UserProfileForm()
+    profile_form = Profile_Form()
+    context = {'user_form': user_form, 'profile_form': profile_form}
+    return render(request, 'home.html', context)
 
 def profile(request):
     return render(request, 'profile.html')
