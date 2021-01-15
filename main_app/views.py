8eb698403e7f1614f417825b36a8fa2a73a5a103
Heymon <rcavalleiro@gmail.com>
2021-01-15 11:02:09 -0800

    adds signup route in views urls and reworks signup html

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
-from main_app.forms import UserProfileForm
+from django.http.request import QueryDict
+from main_app.models import Profile
+from main_app.forms import Profile_Form, UserProfileForm
 from django.shortcuts import render, redirect
 
 from django.contrib.auth.forms import UserCreationForm
@@ -10,15 +12,28 @@ def home (request):
     return render(request, 'home.html')
 
 def signup(request):
-
+    error = ''
     if request.method == 'POST':
-        user_form = UserProfileForm(request.POST)
+        mutable_POST = request.POST.copy()
+        user_country = QueryDict(f'cur_country={mutable_POST.pop("cur_country")[0]}')
+
+        print(request.POST["username"])
+        user_form = UserProfileForm(mutable_POST)
         if user_form.is_valid():
             user = user_form.save()
+            #creates profile/connects to user
+            profile = Profile_Form(user_country)
+            user_profile = profile.save(commit=False)
+            user_profile.user = user
+            user_profile.save()
             login(request, user)
             return redirect('home')
+        else:
+            error = user_form.errors
+            print(error)
     
     user_form = UserProfileForm()
-    context = {'user_form': user_form}
+    profile_form = Profile_Form()
+    context = {'user_form': user_form, 'profile_form': profile_form}
     return render(request, 'registration/signup.html', context)
 
