2ad0954c162c260456719fa066733af6b1cd2a36
bmorataya <bills.morataya@gmail.com>
2021-01-18 18:53:08 -0800

    profile layout

--- main_app/static/scripts/app.js
+++ main_app/static/scripts/app.js
@@ -1,25 +1,5 @@
-//toggle for profile//
-$("[data-toggle='toggle']").click(function() {
-    var selector = $(this).data("target");
-    $(selector).toggleClass('in');
-});
+$('#myList a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  })
 
-//popover for profile)//
-$(function(){
-    $('#post').popover({
-        placement: 'bottom',
-        title: 'Popover Form',
-        html:true,
-        content:  $('#myForm').html()
-    }).on('click', function(){
-    $('.btn-primary').click(function(){
-    $('#result').after("form submitted by " + $('#title').val())
-        $.post('/echo/html/',  {
-            title: $('#title').val(),
-        }, function(r){
-        $('#pops').popover('hide')
-        $('#result').html('resonse from server could be here' )
-        })
-    })
-})
-})
